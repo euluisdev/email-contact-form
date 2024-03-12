fbaaaf1dda832602e2389ebb2f3f19e91d663a36
euluis-dev <fluisf00@gmail.com>
2024-03-12 18:08:48 -0300

    form initialized with bootstrap classes

--- index.php
+++ index.php
@@ -1,15 +1,30 @@
 <!doctype html>
 <html lang="pt-BR">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"
+        defer></script>
     <title>Contato por E-mail</title>
-    
-  </head>
-  <body>
-    <h1>Hello, worldão!</h1>
-    
-  </body>
+
+</head>
+
+<body>
+    <div id="container">
+        <div id="row">
+            <div id="col">
+                <h1 class="my-5">Formulário de Contato</h1>
+                <form action="enviar" method="POST">
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+
 </html>
